97310028a72c852fb6f4b26c1dfcb75de5a961de
Captain Bhargav <phanikiran00081@gmail.com>
2025-07-13 12:53:46 +0530

    Updated

--- Shell_Scripting/Variables/variables.sh
+++ Shell_Scripting/Variables/variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 GREEN='\e[32m'
-yellow='\e[43m'
+BLUE='\e[34m'
 NC='\e[0m' # No colour
 
 echo "Hello Team, I am implementing varibales in this script"
@@ -9,6 +9,6 @@ var1=Bhargav
 var2=sumati
 
 echo -e "$GREEN $var1: Hi $var2 $NC"
-echo -e "$yellow $var2: Hi $var1, How are you $NC"
+echo -e "$BLUE $var2: Hi $var1, How are you $NC"
 echo -e "$GREEN $var1: I am doing good $var2, How are you? $NC"
-echo -e "$yellow $var2: I am good $var1, Thanks for asking $NC"
+echo -e "$BLUE $var2: I am good $var1, Thanks for asking $NC"
