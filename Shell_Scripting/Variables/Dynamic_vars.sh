955c40e48312d86243b2da7c05425bf7543f32f7
Captain Bhargav <phanikiran00081@gmail.com>
2025-07-13 13:17:04 +0530

    Param variables

---
+++ Shell_Scripting/Variables/Dynamic_vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+GREEN='\e[32m'
+BLUE='\e[34m'
+NC='\e[0m' # No colour
+
+echo "Hello Team, I am implementing varibales in this script"
+
+var1=$1
+var2=$2
+
+echo -e "$GREEN $var1: Hi $var2 $NC"
+echo -e "$BLUE $var2: Hi $var1, How are you $NC"
+echo -e "$GREEN $var1: I am doing good $var2, How are you? $NC"
+echo -e "$BLUE $var2: I am good $var1, Thanks for asking $NC"
