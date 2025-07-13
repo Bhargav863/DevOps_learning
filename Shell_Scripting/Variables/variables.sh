63de6134a5f82598aab4601c8a503b95ba7bfb64
Captain Bhargav <phanikiran00081@gmail.com>
2025-07-13 12:49:00 +0530

    Updated shell script

--- Shell_Scripting/Variables/variables.sh
+++ Shell_Scripting/Variables/variables.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 GREEN='\e[32m'
 yellow='\e[43'
+NC='\e[0m' # No colour
 
 echo "Hello Team, I am implementing varibales in this script"
 
 var1=Bhargav
 var2=sumati
 
-echo -e "$GREEN $var1: Hi $var2"
-echo -e "$yellow $var2: Hi $var1, How are you"
-echo -e "$GREEN $var1: I am doing good $var2, How are you?"
-echo -e "$yellow $var2: I am good $var1, Thanks for asking"
+echo -e "$GREEN $var1: Hi $var2 $NC"
+echo -e "$yellow $var2: Hi $var1, How are you $NC"
+echo -e "$GREEN $var1: I am doing good $var2, How are you? $NC"
+echo -e "$yellow $var2: I am good $var1, Thanks for asking $NC"
