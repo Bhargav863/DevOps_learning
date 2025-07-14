f04a163f91ba6084b7ecc94f0fb6901888f3c97f
Captain Bhargav <phanikiran00081@gmail.com>
2025-07-14 11:39:01 +0530

    Updated

--- Shell_Scripting/Conditions and loops/conditions.sh
+++ Shell_Scripting/Conditions and loops/conditions.sh
@@ -6,6 +6,8 @@ Number=$1
 
 if [ $Number -gt 10 ]; then
     echo "$Number is greater than 10"
+elif [ $Number -eq 10 ]; then
+    echo "$Number is equal to 10"
 else
     echo "$Number is less than 10"
 fi
