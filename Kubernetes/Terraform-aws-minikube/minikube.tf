a8d428264614bcaff327f250fb58d6b760440193
Captain Bhargav <bp7445@gmail.com>
2025-08-16 21:24:22 +0530

    Updated Minikube installation

---
+++ Kubernetes/Terraform-aws-minikube/minikube.tf
@@ -0,0 +1,23 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "my-minikube"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "devops.pub"
+  aws_subnet_id = "subnet-07e59a767e6834f6a"
+  ami_image_id = "ami-020cba7c55df1f615"
+  hosted_zone = "381491874932.realhandsonlabs.net"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-dashabord/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-metrics-server/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-nginx-ingress/init.sh"
+  ]
+}
