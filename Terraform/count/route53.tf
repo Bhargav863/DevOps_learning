f3347462c68a365cbdd1234f5be6f35ec3d82397
Captain Bhargav <bp7445@gmail.com>
2025-09-16 22:47:10 +0530

    Updated

--- Terraform/count/route53.tf
+++ Terraform/count/route53.tf
@@ -2,6 +2,7 @@ resource "aws_route53_record" "records" {
   count   = 10
   zone_id = var.zone_id
   name    = "${var.instance_names[count.index]}.${var.domain}" #interpolation
+  # when you mixing variable and string you need to give called as interpolation ${}.string
   type    = "A"
   ttl     = 1
   records = [aws_instance.my_ec2[count.index].private_ip]
